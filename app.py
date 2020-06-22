6cda84d85f3d93ff32633165e97c4d5eb86ecc39
Paulo Marcio <paulom.matias@gmail.com>
2020-06-21 22:16:47 -0300

    Habilitando CORS

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 from resources.impostos.irpf import Irpf
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
+
 
 class Home(Resource):
     def get(self):
